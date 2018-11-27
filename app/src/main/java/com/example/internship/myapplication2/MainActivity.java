7d9fe8248267ec2e2c39240cffe872d0bcc5670a
rezaulkhan111 <rezaulkhan111@hotmail.com>
2018-11-27 14:58:55 +0600

    secound commit

--- app/src/main/java/com/example/internship/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/internship/myapplication2/MainActivity.java
@@ -2,14 +2,23 @@ package com.example.internship.myapplication2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.internship.myapplication2.model.DairySiteAssessment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.et_cross_milch_cow_qut)
     EditText m_et_cross_milch_cow_qut;
-    @BindView(R.id.et_cross_male_cow_price)
+    @BindView(R.id.et_cross_milch_cow_price)
     EditText m_et_cross_milch_cow_price;
     @BindView(R.id.et_cross_milch_cow_age)
     EditText m_et_cross_milch_cow_age;
@@ -38,8 +47,10 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.et_cross_male_cow_qut)
     EditText m_et_cross_male_cow_qut;
+
     @BindView(R.id.et_cross_male_cow_price)
     EditText m_et_cross_male_cow_price;
+
     @BindView(R.id.et_cross_male_cow_age)
     EditText m_et_cross_male_cow_age;
 
@@ -47,12 +58,12 @@ public class MainActivity extends AppCompatActivity {
     EditText m_et_local_milch_cow_qut;
     @BindView(R.id.et_local_milch_cow_price)
     EditText m_et_local_milch_cow_price;
-    @BindView(R.id.et_local_male_cow_age)
+    @BindView(R.id.et_local_milch_cow_age)
     EditText m_et_local_milch_cow_age;
 
     @BindView(R.id.tv_local_total_cow)
     TextView m_tv_local_total_cow;
-    @BindView(R.id.et_local_milch_cow_qut)
+    @BindView(R.id.et_local_calf_cow_qut)
     EditText m_et_local_calf_cow_qut;
     @BindView(R.id.et_local_calf_cow_price)
     EditText m_et_local_calf_cow_price;
@@ -66,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.et_local_male_cow_age)
     EditText m_et_local_male_cow_age;
 
+    ////RadioButton
     @BindView(R.id.rg_cowshed_ans)
     RadioGroup m_rg_cowshed_ans;
     @BindView(R.id.rg_pacca_floor_ans)
@@ -84,60 +96,143 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.rg_training_from_ans)
     RadioGroup m_rg_training_from_ans;
 
-    Spinner spinner;
+    Spinner m_spinner_loan_type;
+    //   @BindView(R.id.btn_save)
+    Button m_btn_save;
+
+    DairySiteAssessment assessment = new DairySiteAssessment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        spinner = findViewById(R.id.spinner_loan_type);
+        //  DeclerEditText();
+        declerSpinner();
 
+        m_btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initializeValue();
+            }
+        });
 
+       // m_et_cross_milch_cow_qut.addTextChangedListener(addValue(10,15,20));
+    }
+
+    private Integer addValue(int mi_Qt, int c_Qt, int ma_Qt) {
+        return mi_Qt + c_Qt + ma_Qt;
+    }
+
+    private void declerSpinner() {
+        m_spinner_loan_type = findViewById(R.id.spinner_loan_type);
+        m_btn_save = findViewById(R.id.btn_save);
+        List<String> categories = new ArrayList<>();
+        categories.add("Select Type");
+        categories.add("Bank");
+        categories.add("Microfinance");
+        categories.add("Not Prepared");
+        categories.add("No Need Loan");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        m_spinner_loan_type.setAdapter(dataAdapter);
+    }
+//    private void DeclerEditText() {
+//        m_et_field_officer_name = findViewById(R.id.et_field_officer_name);
+//        m_et_cbo_no = findViewById(R.id.et_cbo_no);
+//
+//        m_et_cross_milch_cow_qut = findViewById(R.id.et_cross_milch_cow_qut);
+//        m_et_cross_milch_cow_price = findViewById(R.id.et_cross_milch_cow_price);
+//        m_et_cross_milch_cow_age = findViewById(R.id.et_cross_milch_cow_age);
+//
+//        m_et_cross_calf_cow_qut = findViewById(R.id.et_cross_calf_cow_qut);
+//        m_et_cross_calf_cow_price = findViewById(R.id.et_cross_calf_cow_price);
+//        m_et_cross_calf_cow_age = findViewById(R.id.et_cross_calf_cow_age);
+//
+//        m_et_cross_male_cow_qut = findViewById(R.id.et_cross_male_cow_qut);
+//        m_et_cross_male_cow_price = findViewById(R.id.et_cross_male_cow_price);
+//        m_et_cross_male_cow_age = findViewById(R.id.et_cross_male_cow_age);
+//
+//        m_et_local_milch_cow_qut = findViewById(R.id.et_local_milch_cow_qut);
+//        m_et_local_milch_cow_price = findViewById(R.id.et_local_milch_cow_price);
+//        m_et_local_milch_cow_age = findViewById(R.id.et_local_milch_cow_age);
+//
+//        m_et_local_calf_cow_qut = findViewById(R.id.et_local_calf_cow_qut);
+//        m_et_local_calf_cow_price = findViewById(R.id.et_local_calf_cow_price);
+//        m_et_local_calf_cow_age = findViewById(R.id.et_local_calf_cow_age);
+//
+//        m_et_local_male_cow_qut = findViewById(R.id.et_local_male_cow_qut);
+//        m_et_local_male_cow_price = findViewById(R.id.et_local_male_cow_price);
+//        m_et_local_male_cow_age = findViewById(R.id.et_local_male_cow_age);
+//
+//        m_rg_cowshed_ans = findViewById(R.id.rg_cowshed_ans);
+//        m_rg_pacca_floor_ans = findViewById(R.id.rg_pacca_floor_ans);
+//
+//        m_rg_business_activity_ans = findViewById(R.id.rg_business_activity_ans);
+//        m_et_business_activity_comment = findViewById(R.id.et_business_activity_comment);
+//
+//        m_rg_proprietor_have_loan_ans = findViewById(R.id.rg_proprietor_have_loan_ans);
+//        m_et_proprietor_have_loan_comment = findViewById(R.id.et_proprietor_have_loan_comment);
+//
+//        m_rg_training_from_ans = findViewById(R.id.rg_training_from_ans);
+//
+//        m_btn_save = findViewById(R.id.btn_save);
+//    }
+
+    private void initializeValue() {
+        assessment.setFieldOfficer(returnEditTextValue(m_et_field_officer_name));
+        assessment.setCBONo(returnEditTextValue(m_et_cbo_no));
+
+        assessment.setCrossMilchCowQty(Integer.parseInt(returnEditTextValue(m_et_cross_milch_cow_qut)));
+        assessment.setCrossMilchCowPrice(Integer.parseInt(returnEditTextValue(m_et_cross_milch_cow_price)));
+        assessment.setCrossMilchCowAge(Double.parseDouble(returnEditTextValue(m_et_cross_milch_cow_age)));
+
+        assessment.setCrossCalfCowQty(Integer.parseInt(returnEditTextValue(m_et_cross_calf_cow_qut)));
+        assessment.setCrossCalfCowPrice(Integer.parseInt(returnEditTextValue(m_et_cross_calf_cow_price)));
+        assessment.setCrossCalfCowAge(Double.parseDouble(returnEditTextValue(m_et_cross_calf_cow_age)));
+
+        assessment.setCrossMaleCowQty(Integer.parseInt(returnEditTextValue(m_et_cross_male_cow_qut)));
+        assessment.setCrossMaleCowPrice(Integer.parseInt(returnEditTextValue(m_et_cross_male_cow_price)));
+        assessment.setCrossMaleCowAge(Double.parseDouble(returnEditTextValue(m_et_cross_male_cow_age)));
+
+        assessment.setLocalMilchCowQty(Integer.parseInt(returnEditTextValue(m_et_local_milch_cow_qut)));
+        assessment.setLocalMilchCowPrice(Integer.parseInt(returnEditTextValue(m_et_local_milch_cow_price)));
+        assessment.setLocalMilchCowAge(Double.parseDouble(returnEditTextValue(m_et_local_milch_cow_age)));
+
+        assessment.setLocalCalfCowQty(Integer.parseInt(returnEditTextValue(m_et_local_calf_cow_qut)));
+        assessment.setLocalCalfCowPrice(Integer.parseInt(returnEditTextValue(m_et_local_calf_cow_price)));
+        assessment.setLocalCalfCowAge(Double.parseDouble(returnEditTextValue(m_et_local_calf_cow_age)));
+
+        assessment.setLocalMaleCowQty(Integer.parseInt(returnEditTextValue(m_et_local_male_cow_qut)));
+        assessment.setLocalMaleCowPrice(Integer.parseInt(returnEditTextValue(m_et_local_male_cow_price)));
+        assessment.setLocalMaleCowAge(Double.parseDouble(returnEditTextValue(m_et_local_male_cow_age)));
+
+        assessment.setHasOwnerCowshed(returnRadioGroupValue(m_rg_cowshed_ans));
+        assessment.setHasCowshedPaccaFloor(returnRadioGroupValue(m_rg_pacca_floor_ans));
+
+        assessment.setHasOwnerOtherBusiness(returnRadioGroupValue(m_rg_business_activity_ans));
+        assessment.setOtherBusinessName(returnEditTextValue(m_et_business_activity_comment));
+
+        assessment.setHasOwnerCurrentLoan(returnRadioGroupValue(m_rg_proprietor_have_loan_ans));
+        assessment.setLoanInstitutionName(returnEditTextValue(m_et_proprietor_have_loan_comment));
+
+        assessment.setHasTrainingFrom(returnRadioGroupValue(m_rg_training_from_ans));
+        assessment.setSuitableLoanType(m_spinner_loan_type.getSelectedItem().toString());
     }
 
 
-    private void DeclerID() {
-        DairySiteAssessment assessment = new DairySiteAssessment();
+    private String returnEditTextValue(EditText editText) {
+        return editText.getText().toString();
+    }
 
-
-        assessment.setFieldOfficer(String.valueOf(m_et_field_officer_name.getText()));
-        assessment.setCBONo(String.valueOf(m_et_cbo_no.getText()));
-
-        assessment.setCrossMilchCowQty(Integer.parseInt(m_et_cross_male_cow_qut.getText().toString()));
-        assessment.setCrossMilchCowPrice(Integer.parseInt(m_et_cross_male_cow_price.getText().toString()));
-        assessment.setCrossMilchCowAge(Double.parseDouble(m_et_cross_male_cow_age.getText().toString()));
-
-        assessment.setCrossCalfCowQty(Integer.parseInt(m_et_cross_calf_cow_qut.getText().toString()));
-        assessment.setCrossCalfCowPrice(Integer.parseInt(m_et_cross_calf_cow_price.getText().toString()));
-        assessment.setCrossCalfCowAge(Double.parseDouble(m_et_cross_calf_cow_age.getText().toString()));
-
-        assessment.setCrossMaleCowQty(Integer.parseInt(m_et_cross_male_cow_qut.getText().toString()));
-        assessment.setCrossMaleCowPrice(Integer.parseInt(m_et_cross_male_cow_price.getText().toString()));
-        assessment.setCrossMaleCowAge(Double.parseDouble(m_et_cross_male_cow_age.getText().toString()));
-
-        assessment.setLocalMilchCowQty(Integer.parseInt(m_et_local_calf_cow_qut.getText().toString()));
-        assessment.setLocalMilchCowPrice(Integer.parseInt(m_et_local_milch_cow_price.getText().toString()));
-        assessment.setLocalMilchCowAge(Double.parseDouble(m_et_local_milch_cow_age.getText().toString()));
-
-        assessment.setLocalCalfCowQty(Integer.parseInt(m_et_local_calf_cow_qut.getText().toString()));
-        assessment.setLocalCalfCowPrice(Integer.parseInt(m_et_local_calf_cow_price.getText().toString()));
-        assessment.setLocalCalfCowAge(Double.parseDouble(m_et_local_calf_cow_age.getText().toString()));
-
-        assessment.setLocalMaleCowQty(Integer.parseInt(m_et_local_male_cow_qut.getText().toString()));
-        assessment.setLocalMaleCowPrice(Integer.parseInt(m_et_local_male_cow_price.getText().toString()));
-        assessment.setLocalMaleCowAge(Double.parseDouble(m_et_local_male_cow_age.getText().toString()));
-
-        // int l=m_rg_cowshed_ans
-//        assessment.setHasOwnerCowshed();
-//        assessment.setHasCowshedPaccaFloor();
-//
-//        assessment.setHasOwnerOtherBusiness();
-        assessment.setOtherBusinessName(m_et_business_activity_comment.getText().toString());
-
-//        assessment.setHasOwnerCurrentLoan();
-        assessment.setLoanInstitutionName(m_et_proprietor_have_loan_comment.getText().toString());
-
-//        assessment.setSuitableLoanType();
+    private boolean returnRadioGroupValue(RadioGroup radioGroup) {
+        RadioButton button = findViewById(radioGroup.getCheckedRadioButtonId());
+        if (button.getText().toString().equals("Yes")) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
