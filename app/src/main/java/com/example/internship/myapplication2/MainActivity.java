a1db86b23a3c079ce38abc25204ba9b8c9e05716
rezaulkhan111 <rezaulkhan111@hotmail.com>
2018-11-27 18:47:14 +0600

    three commit

--- app/src/main/java/com/example/internship/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/internship/myapplication2/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.internship.myapplication2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -107,8 +108,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        //  DeclerEditText();
         declerSpinner();
 
         m_btn_save.setOnClickListener(new View.OnClickListener() {
@@ -117,12 +116,6 @@ public class MainActivity extends AppCompatActivity {
                 initializeValue();
             }
         });
-
-       // m_et_cross_milch_cow_qut.addTextChangedListener(addValue(10,15,20));
-    }
-
-    private Integer addValue(int mi_Qt, int c_Qt, int ma_Qt) {
-        return mi_Qt + c_Qt + ma_Qt;
     }
 
     private void declerSpinner() {
@@ -181,6 +174,7 @@ public class MainActivity extends AppCompatActivity {
 //        m_btn_save = findViewById(R.id.btn_save);
 //    }
 
+    @SuppressLint("SetTextI18n")
     private void initializeValue() {
         assessment.setFieldOfficer(returnEditTextValue(m_et_field_officer_name));
         assessment.setCBONo(returnEditTextValue(m_et_cbo_no));
@@ -194,8 +188,10 @@ public class MainActivity extends AppCompatActivity {
         assessment.setCrossCalfCowAge(Double.parseDouble(returnEditTextValue(m_et_cross_calf_cow_age)));
 
         assessment.setCrossMaleCowQty(Integer.parseInt(returnEditTextValue(m_et_cross_male_cow_qut)));
+
         assessment.setCrossMaleCowPrice(Integer.parseInt(returnEditTextValue(m_et_cross_male_cow_price)));
         assessment.setCrossMaleCowAge(Double.parseDouble(returnEditTextValue(m_et_cross_male_cow_age)));
+
 
         assessment.setLocalMilchCowQty(Integer.parseInt(returnEditTextValue(m_et_local_milch_cow_qut)));
         assessment.setLocalMilchCowPrice(Integer.parseInt(returnEditTextValue(m_et_local_milch_cow_price)));
@@ -209,6 +205,7 @@ public class MainActivity extends AppCompatActivity {
         assessment.setLocalMaleCowPrice(Integer.parseInt(returnEditTextValue(m_et_local_male_cow_price)));
         assessment.setLocalMaleCowAge(Double.parseDouble(returnEditTextValue(m_et_local_male_cow_age)));
 
+
         assessment.setHasOwnerCowshed(returnRadioGroupValue(m_rg_cowshed_ans));
         assessment.setHasCowshedPaccaFloor(returnRadioGroupValue(m_rg_pacca_floor_ans));
 
@@ -220,8 +217,20 @@ public class MainActivity extends AppCompatActivity {
 
         assessment.setHasTrainingFrom(returnRadioGroupValue(m_rg_training_from_ans));
         assessment.setSuitableLoanType(m_spinner_loan_type.getSelectedItem().toString());
+
+        int value = addValue(m_et_cross_milch_cow_qut, m_et_cross_calf_cow_qut, m_et_cross_male_cow_qut);
+        int value2 = addValue(m_et_local_milch_cow_qut, m_et_local_calf_cow_qut, m_et_local_male_cow_qut);
+        m_tv_cross_total_cow.setText("Cross Cow Total: " + value);
+        m_tv_local_total_cow.setText("Local Cow Total: " + value2);
+        m_tv_current_no_cow.setText("" + (value + value2));
     }
 
+    private Integer addValue(EditText mi_Qt, EditText ca_Qt, EditText ma_Qt) {
+        String Qt1 = mi_Qt.getText().toString();
+        String Qt2 = ca_Qt.getText().toString();
+        String Qt3 = ma_Qt.getText().toString();
+        return Integer.parseInt(Qt1 + Qt2 + Qt3);
+    }
 
     private String returnEditTextValue(EditText editText) {
         return editText.getText().toString();
